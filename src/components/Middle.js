6523fd0f951ee351c43b2e848795ac8e34094b10
hoki0713 <hojeong0713@gmail.com>
2021-12-16 06:54:56 +0900

    Improve mobile design in Middle button and icons

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -1,22 +1,21 @@
 import { Row, Col, Container } from 'react-bootstrap';
 
 import { LinkedIn, Facebook, Mail, Twitter } from './UI/Icons';
-
 import ApplyButton from './UI/Button/Apply';
 
 function Middle(props) {
 	return (
 		<Container fluid className="bg-white pt-5 pb-5">
-			<Row className="justify-content-md-center">
-				<Col sm="12" lg="5" className="text-center d-grid gap-4">
+			<Row className="justify-content-center">
+				<Col sm="8" md="6" lg="5" className="text-center d-grid gap-4">
 					<h3>Se hur du matchar med det här jobbet</h3>
 					<p>
 						I din ansökan kommer du att få svara på frågor. Då kan vi matcha
 						dina kunskaper och personliga egenskaper med rollen och
 						arbetsplatsens kultur.
 					</p>
-					<ApplyButton className="w-25 m-auto">Ansök Här</ApplyButton>
-					<div className="d-flex flex-wrap justify-content-center gap-5">
+					<ApplyButton className="px-5 m-auto">Ansök Här</ApplyButton>
+					<div className="d-flex flex-wrap justify-content-around">
 						<LinkedIn />
 						<Facebook />
 						<Mail />
