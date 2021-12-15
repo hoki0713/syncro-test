fde78807b9b72c21cb9019936f69ce8d327e73cd
hoki0713 <hojeong0713@gmail.com>
2021-12-16 06:57:16 +0900

    Improve mobile design in CompanyDetail link and button

--- src/components/CompanyDetail.js
+++ src/components/CompanyDetail.js
@@ -9,10 +9,10 @@ function CompanyDetail(props) {
 				<Col sm="12" lg="7" className="text-center d-grid gap-3">
 					<h3>Om arbetsgivaren</h3>
 					<p>{companyDataObj.about}</p>
-					<Button variant="link" className="link w-25 mx-auto fs-4">
+					<Button variant="link" className="link mx-auto fs-4">
 						{companyDataObj.url}
 					</Button>
-					<Button variant="primary" className="col-5 mx-auto">
+					<Button variant="primary" className="mx-auto px-5">
 						{`Fler jobb hos ${companyDataObj.company}`}
 					</Button>
 				</Col>
